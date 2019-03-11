3357d638a1ae44fbf862ff8cdf2e072af5adbaac
jtew396 <jtew396@gmail.com>
2019-03-11 16:18:50 -0500

    Wow. I think this file actually worked.

--- import.py
+++ import.py
@@ -3,24 +3,20 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv(""))                                           # database engine object from SQLAlchemy that manages connections to the database
+engine = create_engine(os.getenv("DATABASE_URL"))                               # database engine object from SQLAlchemy that manages connections to the database
                                                                                 # DATABASE_URL is an environment variable that indicates where the database lives
 db = scoped_session(sessionmaker(bind=engine))                                  # create a 'scoped session' that ensures different users' interactions with the
                                                                                 # database are kept separate
 
 def main():
-    table = db.execute("CREATE TABLE books (
-        id SERIAL PRIMARY KEY,
-        isbn VARCHAR NOT NULL,
-        title VARCHAR NOT NULL,
-        author VARCHAR NOT NULL,
-        year VARCHAR NOT NULL))
+    table = db.execute("CREATE TABLE books (id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year VARCHAR NOT NULL)")
+    # Create the table called books and name each column
     f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:                                    # Loop gives each column a name
-        db.execute("INSERT INTO books (isbn, title, author, year) VALUES
-            (:isbn, :title, :author, :year)",{"isbn":isbn, "title":title,
-            "author":author, "year":year})                                      # Substitute values from the CSV line into SQL command, as per books.CSV
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",{"isbn":isbn, "title":title, "author":author, "year":year})
+        # Substitute values from the CSV line into SQL command, as per books.CSV
+        print(f"Added {title} by {author}.")
     db.commit()
 
 
