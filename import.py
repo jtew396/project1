e0f2e031177fa58e95743772a12cf1e4166b0e80
jtew396 <jtew396@gmail.com>
2019-03-11 12:14:49 -0500

    Adding to the import.py

--- import.py
+++ import.py
@@ -3,7 +3,10 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv(""))
+engine = create_engine(os.getenv(""))               # database engine object from SQLAlchemy that manages connections to the database
+                                                    # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))      # create a 'scoped session' that ensures different users' interactions with the
+                                                    # database are kept separate
 
 def main():
     f = open("books.csv")
