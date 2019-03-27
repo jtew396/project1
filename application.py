6d0760d584f179999c7385ef7581bda6f1c1438e
jtew396 <jtew396@gmail.com>
2019-03-26 21:32:16 -0500

    Adding more to register.

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -27,10 +29,57 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """ Allow users to register for the Book Review Website """
 
+    # Forget any user_id
+    session.clear()
+
+    # User reached by submitting a post request
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Missing username!", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Missing password!", 400)
+
+        # Ensure confirmation was submitted
+        elif not request.form.get("confirmation"):
+            return apology("Must input password twice!", 400)
+
+        # Ensure confirmation matches password
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return apology("Passwords must match!", 400)
+
+        # Store the username in the database
+        result = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+                            username=request.form.get("username"),
+                            hash=generate_password_hash(request.form.get("password")))
+        if not result:
+            return apology("Username currently in use", 400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+
+        states = db.execute("SELECT * FROM states")
+
+        return render_template("register.html", states=states)
+
+    # If for some reason we get to this point
     return render_template("register.html")
 
 @app.route("/login", methods=["GET"])
